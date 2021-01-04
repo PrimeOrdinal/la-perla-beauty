d56d48aa8a814b972997b24359bcf048aedd408d
Karl Podger <karl.podger@primeordinal.com>
2021-01-04 10:55:41 +0000

    Test using an SVG

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,8 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+import Logo from "../images/logo.svg";
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
@@ -14,6 +16,7 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
+    <Logo />
     <Link to="/product-listings/">Go to product listings</Link> <br />
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
