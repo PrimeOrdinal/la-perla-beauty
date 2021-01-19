4f242e47c582750f561d925aa08f030f5489bcfb
Karl Podger <karl.podger@primeordinal.com>
2021-01-19 11:50:45 +0000

    Configure linters with up-to-date configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,10 @@ const path = require("path")
 
 module.exports = {
   "extends": [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "plugin:prettier/recommended"
+    "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
+    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    "plugin:prettier/recommended" // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   "ignorePatterns": ["**/*.js"],
   "overrides": [
@@ -19,18 +17,17 @@ module.exports = {
     }
   ],
   "parser": "@typescript-eslint/parser",
-  // "parserOptions": {
-  //   "ecmaFeatures": {
-  //     "jsx": true
-  //   },
-  //   "ecmaVersion": 2020,
-  //   "project": ["./tsconfig.json"],
-  //   "sourceType": "module",
-  //   "tsconfigRootDir": __dirname
-  // },
-  "plugins": ["graphql", "react", "@typescript-eslint", "prettier"],
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: "module", // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true // Allows for the parsing of JSX
+    }
+  },
+  "plugins": ["formatjs", "graphql", "react", "@typescript-eslint", "prettier"],
   "root": true,
   "rules": {
+    "formatjs/no-offset": "error",
     "graphql/template-strings": [
       "error",
       {
@@ -51,4 +48,4 @@ module.exports = {
       "version": "detect"
     }
   }
-}
+}  
