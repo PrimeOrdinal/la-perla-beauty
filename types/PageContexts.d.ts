706bd7c60f8a9f6ed54ebe3a870b949662516e8c
Karl Podger <karl.podger@primeordinal.com>
2021-01-19 12:24:53 +0000

    Rename custom types to avoid clashes with library code

--- types/PageContexts.d.ts
+++ types/PageContexts.d.ts
@@ -11,5 +11,3 @@ type PageContextTypeBreadcrumb = {
     hiddenCrumbs?: string[]
   }
 }
-
-
--- types/global.d.ts
+++ types/global.d.ts
@@ -2,9 +2,7 @@ export {}
 
 declare global {
   namespace React {
-    // interface ReactDOMAttributes {
-    //   content?: string
-    // }
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     interface HTMLAttributes<T> {
       content?: boolean | number | string | undefined
     }
