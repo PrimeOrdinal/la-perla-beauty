c0e7622027d17c0f661d0f2d5b63702afcc15dc0
Karl Podger <karl.podger@primeordinal.com>
2021-01-20 11:43:31 +0000

    Add decorator to apply theme to Storybook components

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,19 @@
+// See https://www.learnstorybook.com/design-systems-for-developers/react/en/build/
+
+import { ThemeProvider } from 'styled-components';
+
+import { GlobalStyle } from '../src/styles/GlobalStyle';
+import { theme } from "../src/theme"
+
+// Global decorator to apply the styles to all stories
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Story />
+    </ThemeProvider>
+  ),
+];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
