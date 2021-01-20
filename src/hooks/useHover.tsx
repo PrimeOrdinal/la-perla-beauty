8c0f231f1bcf4fc651582247d8c3d49870017bb4
Karl Podger <karl.podger@primeordinal.com>
2021-01-20 23:19:41 +0000

    Remove unused hook argument

--- src/hooks/useHover.tsx
+++ src/hooks/useHover.tsx
@@ -11,20 +11,17 @@ export function useHover(): [
   const handleMouseOver = () => setValue(true)
   const handleMouseOut = () => setValue(false)
 
-  useEffect(
-    () => {
-      const node = ref.current
+  useEffect(() => {
+    const node = ref.current
 
-      node?.addEventListener("mouseover", handleMouseOver)
-      node?.addEventListener("mouseout", handleMouseOut)
+    node?.addEventListener("mouseover", handleMouseOver)
+    node?.addEventListener("mouseout", handleMouseOut)
 
-      return () => {
-        node?.removeEventListener("mouseover", handleMouseOver)
-        node?.removeEventListener("mouseout", handleMouseOut)
-      }
-    },
-    [ref.current] // Recall only if ref changes
-  )
+    return () => {
+      node?.removeEventListener("mouseover", handleMouseOver)
+      node?.removeEventListener("mouseout", handleMouseOut)
+    }
+  })
 
   return [ref, value]
 }
