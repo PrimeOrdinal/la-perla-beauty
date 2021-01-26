c6611c1954216d18d4d49e99c8e5661398d5e000
Karl Podger <karl.podger@primeordinal.com>
2021-01-26 15:57:01 +0000

    Modify stories shown in Storybook

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,18 +1,18 @@
 import React from "react"
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
-import { Story, Meta } from "@storybook/react/types-6-0"
+import { Story, Meta } from "@storybook/react"
 
-import { Button } from "../components/Button"
+import { Button, ButtonProps } from "../components/Button"
 
 export default {
-  title: "Example/Button",
+  title: "Button",
   component: Button,
   argTypes: {
     bg: { control: "color" },
   },
 } as Meta
 
-const Template: Story = args => <Button {...args} />
+const Template: Story<ButtonProps> = args => <Button {...args} />
 
 export const Primary = Template.bind({})
 Primary.args = {
