2cfa8c897b627e3994ed0567e8a836d2031456c9
Khalid <kmnahari89@gmail.com>
2021-02-03 09:55:34 +0000

    Set root element font size to 16px

--- src/styles/html.tsx
+++ src/styles/html.tsx
@@ -2,7 +2,7 @@ import { mediaQueries } from "../theme"
 export const html = `
     html {
         font-family: "Quicksand", Helvetica, Arial, sans-serif;
-        font-size: 13px;
+        font-size: 16px;
         letter-spacing: 1px;
         line-height: 17px;
     }
