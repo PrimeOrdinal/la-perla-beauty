e6be7750437d8d7c5a7a0095483a670d353b3861
Karl Podger <karl.podger@primeordinal.com>
2021-02-04 16:58:52 +0000

    Add WIP ViewSelector component

---
+++ src/components/ViewSelector.tsx
@@ -0,0 +1,37 @@
+import { themeGet } from "@styled-system/theme-get"
+import clsx from "clsx"
+import React from "react"
+import styled from "styled-components"
+
+import { ReactComponent as Grid } from "../images/Grid.svg"
+import { ReactComponent as List } from "../images/List.svg"
+
+const StyledButton = styled.button`
+  align-items: center;
+  display: grid;
+  font-size: ${themeGet("fontSizes.body")}px;
+  gap: 1rem;
+  grid-auto-flow: column;
+  padding: ${themeGet("space.5")}px ${themeGet("space.8")}px;
+  text-transform: uppercase;
+
+  svg {
+    width: ${themeGet("space.6")}px
+  }
+`
+
+const ContainerStyled = styled.nav`
+  display: grid;
+  grid-auto-flow: column;
+  justify-content: start;
+`
+
+export const ViewSelector: React.FC = () => {
+  return (
+    <ContainerStyled>
+      <StyledButton className={clsx("button--link")} grid>Grid<Grid /></StyledButton>
+      <StyledButton className={clsx("button--link", "current")} list>List<List />
+      </StyledButton>
+    </ContainerStyled>
+  )
+}
