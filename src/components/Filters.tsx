5a67d49f93cfea9207bb0f3fef507c3606bcc0f7
Khalid <kmnahari89@gmail.com>
2021-02-09 17:23:09 +0000

    Refine Filter styling

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,26 +1,39 @@
 import React from "react"
 import styled from "styled-components"
+import { mediaQueries, theme } from "../theme"
 
 import { ReactComponent as Filter } from "../images/Filter.svg"
 import { ReactComponent as Grid } from "../images/Grid.svg"
 import { ReactComponent as List } from "../images/List.svg"
 
 const FilterSection = styled.section`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding-block-end: 1rem;
-  .view {
+  .wrap {
     display: flex;
-    gap: 1rem;
+    justify-content: space-between;
     align-items: center;
-    span {
-      text-transform: uppercase;
-    }
-    svg {
-      height: 16px;
-      object-fit: contain;
-      cursor: pointer;
+    padding-block-end: 1.25rem;
+    border-bottom: ${theme.border.width} solid ${theme.border.color};
+    .view {
+      display: flex;
+      gap: 1rem;
+      align-items: center;
+      span {
+        text-transform: uppercase;
+        &:last-child {
+          display: none;
+          ${mediaQueries.md} {
+            display: block;
+          }
+        }
+      }
+      button {
+        padding: 0;
+        svg {
+          height: 16px;
+          object-fit: contain;
+          cursor: pointer;
+        }
+      }
     }
   }
 `
@@ -28,16 +41,24 @@ const FilterSection = styled.section`
 const Filters = () => {
   return (
     <FilterSection className="container">
-      <div className="view">
-        <span>Grid</span>
-        <Grid />
-        <span>List</span>
-        <List />
-      </div>
-      <div className="view">
-        <span>Refine</span>
-        <Filter />
-        <span>14 Products</span>
+      <div className="wrap">
+        <div className="view">
+          <span>Grid</span>
+          <button>
+            <Grid />
+          </button>
+          <span>List</span>
+          <button>
+            <List />
+          </button>
+        </div>
+        <div className="view">
+          <span>Refine</span>
+          <button>
+            <Filter />
+          </button>
+          <span>14 Products</span>
+        </div>
       </div>
     </FilterSection>
   )
