35638fb3f1c984673f7e7e4d1fa71371dea54fba
Karl Podger <karl.podger@primeordinal.com>
2021-02-10 16:54:01 +0000

    Align button content to center

--- src/styles/button.tsx
+++ src/styles/button.tsx
@@ -12,15 +12,17 @@ export const button = `
         font-family: "Quicksand";
         font-size: var(--font-size-body, 13px);
         gap: 1rem;
+        justify-content: center;
+        justify-items: center;
         letter-spacing: 1px;
         padding: ${theme.space[4]}px ${theme.space[6]}px;
         text-align: center;
         text-transform: uppercase;
-        
+
         &:disabled {
             cursor: not-allowed;
         }
-        
+
         ${theme.mediaQueries.md} {
             padding: ${theme.space[3]}px ${theme.space[4]}px;
         }
