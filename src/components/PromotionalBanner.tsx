a2c2081c15961869a10751cee9e006ea517c85e5
Khalid <kmnahari89@gmail.com>
2021-02-12 17:10:42 +0000

    Add 4 variation of promo banners

--- src/components/PromotionalBanner.tsx
+++ src/components/PromotionalBanner.tsx
@@ -6,18 +6,16 @@ import { compose, layout, space, LayoutProps, SpaceProps } from "styled-system"
 
 import { mediaQueries } from "../theme"
 
-const PromotionalBannerStyled = styled.aside`
-  background-color: ${themeGet("colors.pink")};
+const LayoutBase = styled.aside`
+  background-color: ${props =>
+    props.color ? themeGet(`colors.${props.color}`) : themeGet("colors.pink")};
   border-radius: ${themeGet("radii.4")}px;
   display: grid;
-  grid-auto-flow: row;
   overflow: hidden;
-
   img {
     aspect-ratio: 21 / 14;
     object-fit: cover;
     width: 100%;
-
     ${mediaQueries.md} {
       aspect-ratio: 23 / 8;
       aspect-ratio: 2 / 1;
@@ -25,33 +23,71 @@ const PromotionalBannerStyled = styled.aside`
   }
 
   div {
-    padding-block-end: ${themeGet("space.9")}px;
-    padding-block-start: ${themeGet("space.9")}px;
-    padding-inline-end: ${themeGet("space.9")}px;
-    padding-inline-start: ${themeGet("space.9")}px;
-  }
-  
-  .heading {
-    margin-block-start: unset;
-    padding-block-end: ${themeGet("space.4")}px;
-  }
+    h1 {
+      font-size: 20px;
+      color: inherit;
+      ${mediaQueries.md} {
+        font-size: 24px;
+      }
+    }
 
-  span {
-    display: block;
-    margin-block-end: ${themeGet("space.5")}px;
-  }
+    span {
+      font-size: 13px;
+      color: inherit;
+      ${mediaQueries.md} {
+        font-size: 14px;
+      }
+    }
 
-  .link {
-    color: inherit;
-    text-decoration: unset;
-    text-transform: uppercase;
+    a {
+      font-size: 13px;
+      color: inherit;
+      text-decoration: unset;
+      text-transform: uppercase;
+      font-weight: 600;
+      display: block;
+      margin-block-start: ${themeGet("space.6")}px;
+      padding-block-end: ${themeGet("space.6")}px;
+      ${mediaQueries.md} {
+        font-size: 14px;
+      }
+    }
   }
-
   ${compose(layout, space)}
+`
+const PromotionalBannerView = styled(LayoutBase)`
+  //prop: inlineView = When text is inside image
+  //prop: desktopFull = The reusable hero component used on other pages
+  //prop: listView = The promo banner that spans full width. Is two columns - img & content
+  //default style is the column that spans two which has pink bg with image above
+  position: ${props => (props.inlineView ? "relative" : "static")};
+  border-radius: ${props => (props.desktopFull ? "0px" : "10px")};
+  background-color: ${props =>
+    props.desktopFull ? "transparent" : themeGet(`colors.${props.color}`)};
+  grid-auto-flow: ${props => (props.listView ? "column" : "row")};
+  place-items: ${props => (props.listView ? "center" : "unset")};
+  img {
+    border-radius: ${props => (props.desktopFull ? "10px" : "0px")};
+  }
+  div {
+    color: ${props => (props.inlineView ? "whitesmoke" : "inherit")};
+    position: ${props => (props.inlineView ? "absolute" : "static")};
+    left: 0;
+    right: 0;
+    bottom: ${themeGet("space.7")}px;
+    padding: ${props => {
+      if (props.listView) {
+        return "120px"
+      } else if (props.desktopFull) {
+        return "0px"
+      } else return "20px"
+    }};
+  }
 `
 
 export type PromotionalBannerProps = LayoutProps &
   SpaceProps & {
+    color: "beige" | "pink" | "lightgreen"
     description: string
     image: {
       alt: string
@@ -73,9 +109,7 @@ export const PromotionalBanner: React.FC<PromotionalBannerProps> = ({
   title,
   ...props
 }) => (
-  <PromotionalBannerStyled
-    {...props}
-  >
+  <PromotionalBannerView {...props}>
     {showImage && (
       <img
         alt={image?.description as string}
@@ -84,9 +118,9 @@ export const PromotionalBanner: React.FC<PromotionalBannerProps> = ({
       />
     )}
     <div>
-      <h1 className="heading">{title}</h1>
+      <h1>{title}</h1>
       <span>{text}</span>
-      <Link to={link?.href} className="link">{link?.text}</Link>
+      <Link to={link?.href}>{link?.text}Learn More</Link>
     </div>
-  </PromotionalBannerStyled>
+  </PromotionalBannerView>
 )
