5cbc2a302dea4d09a4bf38fa50f3d4eee7b8568c
Karl Podger <karl.podger@primeordinal.com>
2021-02-15 15:33:49 +0000

    Remove unused custom property definitions

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,8 +20,6 @@ import { MiniBag } from "./MiniBag"
 import { QuickSearch } from "./QuickSearch"
 
 const HeaderStyled = styled.header`
-  --header-min-height: 64px;
-
   background-color: #ffffff;
   border-bottom-style: solid;
   margin-block-end: ${themeGet("space.6")}px;
@@ -32,8 +30,6 @@ const HeaderStyled = styled.header`
   z-index: 999;
 
   ${mediaQueries.md} {
-    --header-min-height: 112px;
-
     padding-block-start: ${themeGet("space.8")}px;
   }
 
