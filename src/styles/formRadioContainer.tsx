145f2008f5a1ad9e0c920e3b0dce9371b8a34781
Khalid <kmnahari89@gmail.com>
2021-02-15 19:07:23 +0000

    Refine radio button styling for product card

--- src/styles/formRadioContainer.tsx
+++ src/styles/formRadioContainer.tsx
@@ -1,4 +1,4 @@
-import { theme } from "../theme"
+import { theme, mediaQueries } from "../theme"
 
 export const formRadioContainer = `
 .field {
@@ -18,7 +18,10 @@ export const formRadioContainer = `
   cursor: pointer;
   position: relative;
   width: 100%;
+  font-size: 13px;
+  ${mediaQueries.md}{
   font-size: 14px;
+  }
   text-transform: uppercase;
   font-weight: normal;
 
@@ -48,7 +51,7 @@ label.fancy-radio-label:after {
   height: 12px;
   content: " ";
   opacity: 0;
-  background: black;
+  background: #363139;
   border-radius: 2px;
   border: 1px solid white;
   transform: translateY(-50%);
@@ -58,6 +61,53 @@ input:checked + label.fancy-radio-label:after {
   opacity:1;
 }
 input:checked + label.fancy-radio-label {
+  font-weight: 600;
+}
+
+
+
+
+
+.fancy-product {
+  clip: rect(1px, 1px, 1px, 1px);
+  position: absolute !important;
+  height: 1px;
+  width: 1px;
+  overflow: hidden;
+}
+
+label.product-radio-label {
+display: inline-block;
+cursor: pointer;
+position: relative;
+font-size: 13px;
+${mediaQueries.md}{
+  font-size: 14px;
+}
+text-transform: unset;
+padding-block-end: 0.5rem;
+font-weight: normal;
+}
+
+
+label.product-radio-label:after {
+  position: absolute;
+  right: 0px;
+  bottom: 0px;
+  display: inline-block;
+  width: 100%;
+  height: 1px;
+  background: #363139;
+  content: " ";
+  opacity: 0;
+}
+
+
+
+input:checked + label.product-radio-label:after  {
+  opacity:1;
+}
+input:checked + label.product-radio-label  {
   font-weight: 600;
 }
 `
