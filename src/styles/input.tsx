f679334f5da2bbf8c670eb28c3dc442367df0770
Khalid <kmnahari89@gmail.com>
2021-02-18 17:50:30 +0000

    Fix input font-sizes and padding. Add focus state globally.

--- src/styles/input.tsx
+++ src/styles/input.tsx
@@ -1,23 +1,25 @@
 import { theme } from "../theme"
+import { themeGet } from "@styled-system/theme-get"
 
 export const input = `
     input {
         border-radius: ${theme.radii[3]}px;
         border-style: solid;
         font-family: "Quicksand";
-        font-size: ${theme.fontSizes[0]}px;
+        font-size: ${theme.fontSizes[2]}px;
         gap: ${theme.space[3]}px;
         padding: ${theme.space[4]}px ${theme.space[7]}px;
 
         ${theme.mediaQueries.md} {
-            font-size: ${theme.fontSizes[2]}px;
-            padding: ${theme.space[3]}px ${theme.space[8]}px;
+            font-size: ${theme.fontSizes[3]}px;
+            padding: ${theme.space[4]}px ${theme.space[8]}px;
         }
 
         &:focus {
             background: none;
             box-shadow: none;
             outline: none;
+            border: 1px solid ${themeGet("colors.black")};
         }
 
         &::selection {
@@ -25,5 +27,6 @@ export const input = `
             box-shadow: none;
             outline: none;
         }
-    }   
+    }
+      
 `
