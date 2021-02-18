a8cab0c5174aa053482212ea50cfcef5f710f018
Karl Podger <karl.podger@primeordinal.com>
2021-02-18 20:06:04 +0000

    Display feedback on QuickBuy "add to bag" button

--- src/components/QuickBuy.tsx
+++ src/components/QuickBuy.tsx
@@ -166,7 +166,7 @@ export const QuickBuy: React.FC<QuickBuyProps> = ({
                   />
                 )}
               </span>{" "}
-              | <span>Add to bag</span>
+              | <span>{isInBag ? "Added to bag" : "Add to bag"}</span>
             </Button>
           </Form>
         </Formik>
