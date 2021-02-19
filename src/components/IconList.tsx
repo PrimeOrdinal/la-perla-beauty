b449283578165828fa5bb06d13389473e8dea1b1
Khalid <kmnahari89@gmail.com>
2021-02-19 15:03:08 +0000

    Create Icon list component to accept props

--- src/components/IconList.tsx
+++ src/components/IconList.tsx
@@ -1,68 +1,64 @@
+import clsx from "clsx"
 import React from "react"
 import styled from "styled-components"
-import { themeGet } from "@styled-system/theme-get"
-import { ReactComponent as WishlistUSP } from "../images/WishlistUSP.svg"
-import { ReactComponent as Checkout } from "../images/Checkout.svg"
-import { ReactComponent as Diamond } from "../images/Diamond.svg"
-import { ReactComponent as Tracking } from "../images/Tracking.svg"
-
-const IconContainerStyled = styled.div`
+import {
+  compose,
+  layout,
+  position,
+  space,
+  LayoutProps,
+  PositionProps,
+  SpaceProps,
+  VariantProps,
+} from "styled-system"
+export type IconListProps = LayoutProps &
+  PositionProps &
+  SpaceProps &
+  VariantProps & {
+    items: Array<{
+      color: "red" | "green" | "blue"
+      heading: string
+      icon: "diamond" | "heart"
+      body: React.ReactNode
+    }>
+    orientation: "horizontal" | "vertical"
+  }
+export const IconListStyled: React.FC<IconListProps> = styled.ul`
   display: grid;
-  grid-auto-flow: row;
-  justify-items: flex-start;
-  gap: ${themeGet("space.7")}px;
-  .iconWrapper {
-    display: grid;
-    grid-auto-flow: column;
-    gap: 1rem;
-    align-items: center;
-    h3 {
-      font-family: "Quicksand";
-      text-transform: uppercase;
-      font-size: ${themeGet("fontSizes.heading3Desktop")}px;
-      display: ${props => (props.withTitle ? "initial" : "none")};
-      text-align: ${props => (props.withTitle ? "center" : "left")};
+  grid-auto-flow: ${props =>
+    props.orientation === "horizontal" ? "column" : "row"};
+  .icon {
+    &.diamond {
+      background-image: url("diamond.svg");
     }
-    span {
-      display: ${props => (props.withTitle ? "block" : "initial")};
+    &.heart {
+      background-image: url("heart.svg");
     }
-
-    svg {
-      height: 40px;
+    &.red {
+      background-color: #ff0000;
     }
-    p {
-      text-align: ${props => (props.withTitle ? "center" : "left")};
+    &.green {
+      background-color: #00ff00;
+    }
+    &.blue {
+      background-color: #0000ff;
     }
   }
+  ${compose(layout, position, space)}
 `
-
-export type IconListProps = {
-  layout: "withTitle"
-}
-
-export const IconList: React.FC<IconListProps> = () => {
-  return (
-    <IconContainerStyled>
-      <div className="iconWrapper">
-        <Checkout />
-        <h3>Title</h3>
-        <span>Lorem ipsum dolor sit amet, consectetur adipisicing.</span>
-      </div>
-      <div className="iconWrapper">
-        <Diamond />
-        <h3>Title</h3>
-        <span>Lorem ipsum dolor sit amet, consectetur adipisicing.</span>
-      </div>
-      <div className="iconWrapper">
-        <Tracking />
-        <h3>Title</h3>
-        <span>Lorem ipsum dolor sit amet, consectetur adipisicing.</span>
-      </div>
-      <div className="iconWrapper">
-        <WishlistUSP />
-        <h3>Title</h3>
-        <span>Lorem ipsum dolor sit amet, consectetur adipisicing.</span>
-      </div>
-    </IconContainerStyled>
-  )
-}
+export const IconList: React.FC<IconListProps> = ({ items, ...props }) => (
+  <IconListStyled {...props}>
+    {items?.length &&
+      items.map((item, index) => (
+        <li key={index}>
+          {item.icon && (
+            <div className={clsx("icon", item.color, item.icon)}>
+              {item.icon}
+            </div>
+          )}
+          {item.heading && <h3>{item.heading}</h3>}
+          {item.body}
+        </li>
+      ))}
+  </IconListStyled>
+)
