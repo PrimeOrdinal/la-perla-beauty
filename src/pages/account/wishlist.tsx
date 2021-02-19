58bfdf3611d271eb960ed2290300631ad16a2066
Karl Podger <karl.podger@primeordinal.com>
2021-02-19 15:01:44 +0000

    Add sample page under account section

---
+++ src/pages/account/wishlist.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+import { Layout } from "../../components/Layout"
+import { SEO } from "../../components/SEO"
+
+const WishlistPage: React.FC = () => (
+  <Layout>
+    <SEO title="My wishlist" />
+    <h1>My wishlist</h1>
+    <p>Content here</p>
+  </Layout>
+)
+
+export default WishlistPage
