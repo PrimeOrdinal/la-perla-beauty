fe558e91f4afc564e48f3b3eb2984326fba0c445
Khalid <kmnahari89@gmail.com>
2021-02-21 11:12:55 +0000

    Create product card component for Orders page

---
+++ src/components/OrdersItem.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+export const OrdersItem = () => {
+  return (
+    <aside>
+      <img src="" alt="image" />
+      <div>order</div>
+      <div className="items">
+        <button>view order details</button>
+        <button>track order</button>
+      </div>
+    </aside>
+  )
+}
