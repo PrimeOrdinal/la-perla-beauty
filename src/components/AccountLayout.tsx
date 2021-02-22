62cd8f0e4e3d0a253c6a134e03f276ec52a9addf
Khalid <kmnahari89@gmail.com>
2021-02-22 22:15:54 +0000

    Refine the reusable layout nav for account page

--- src/components/AccountLayout.tsx
+++ src/components/AccountLayout.tsx
@@ -1,5 +1,6 @@
-import React from "react"
+import React, { useState } from "react"
 import styled from "styled-components"
+import { themeGet } from "@styled-system/theme-get"
 import { ReactComponent as Profile } from "../images/Profile.svg"
 import { ReactComponent as Newsletter } from "../images/Newsletter.svg"
 import { ReactComponent as Shipping } from "../images/Shipping.svg"
@@ -17,6 +18,9 @@ const MainStyled = styled.main`
   gap: 1rem;
   ${mediaQueries.md} {
     grid-auto-flow: column;
+    align-items: flex-start;
+    gap: 5rem;
+    grid-template-columns: minmax(200px, 300px) auto;
   }
   .section-title {
     font-family: "Quicksand";
@@ -37,52 +41,83 @@ const AccountNav = styled(ListPlain)`
   padding: 1rem;
   border-radius: 15px;
   justify-items: flex-start;
-
+  place-content: start;
+  ${mediaQueries.md} {
+    border: none;
+    align-items: flex-start;
+    align-content: flex-start;
+    place-content: unset;
+  }
   li {
+    ${mediaQueries.md} {
+      border-bottom: ${themeGet("border.width")} solid
+        ${themeGet("border.color")};
+      width: 100%;
+      padding: 0.25rem 0;
+    }
     a {
       display: grid;
       grid-auto-flow: column;
       gap: 1rem;
       align-items: center;
       text-transform: uppercase;
+      ${mediaQueries.md} {
+        justify-content: start;
+      }
       svg {
         height: auto;
         width: 15px;
       }
     }
   }
+
+  .active {
+    border-bottom: black solid 1px;
+  }
+  .border {
+    border-bottom: solid ${themeGet("border.width")} ${themeGet("border.color")};
+  }
 `
 
+const activeStyles = {
+  background: "red",
+}
+
 export const AccountLayout = ({ children }) => {
+  const [active, setActive] = useState(false)
+  const handleClick = () => {
+    setActive(!active)
+  }
+
   return (
     <MainStyled className="container">
       <h1 className="section-title">Navigate to section</h1>
       <AccountNav>
         <li>
-          <Link to="information">
+          <Link to="information" activeStyle={activeStyles}>
             <Profile />
             Account information
           </Link>
         </li>
         <li>
-          <Link to="orders">
+          <Link to="orders" activeStyle={activeStyles}>
             <Shipping />
             Orders
           </Link>
         </li>
         <li>
-          <Link>
+          <Link activeStyle={{ color: "red" }}>
             <Return />
             Returns
           </Link>
         </li>
-        <li>
+        <li className={active ? "active" : ""}>
           <Link>
             <Address />
             Address book
           </Link>
         </li>
-        <li>
+        <li className={active ? "active" : ""}>
           <Link to="wishlist">
             <WishlistAbsent />
             Wishlist
