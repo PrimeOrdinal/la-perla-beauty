0d0dfe4c33164f230a50f9d934f8f57a495c1bd3
Khalid <kmnahari89@gmail.com>
2021-02-22 22:13:20 +0000

    Add styling and populate orders page

--- src/pages/account/orders.tsx
+++ src/pages/account/orders.tsx
@@ -1,26 +1,79 @@
 import React from "react"
+import styled from "styled-components"
+import { themeGet } from "@styled-system/theme-get"
 
 import { Layout } from "../../components/Layout"
+import { ListPlain } from "../../components/ListPlain"
 import { OrdersItem } from "../../components/OrdersItem"
 import { AccountLayout } from "../../components/AccountLayout"
 import { SEO } from "../../components/SEO"
 
+const OrdersColumn = styled.section`
+  h1 {
+    text-transform: capitalize;
+    font-size: 32px;
+    margin: 0;
+    font-weight: 300;
+  }
+  .current-orders {
+    h2 {
+      font-size: 24px;
+    }
+    display: grid;
+    grid-auto-flow: row;
+    gap: 1rem;
+    padding-block-end: 2rem;
+  }
+  .previous-orders {
+    display: grid;
+    grid-auto-flow: row;
+    gap: 1rem;
+    border-top: ${themeGet("border.width")} solid ${themeGet("border.color")};
+  }
+
+  .grid-item {
+    display: grid;
+    grid-auto-flow: row;
+    gap: 1rem;
+  }
+`
+
 const OrdersPage: React.FC = () => (
   <Layout>
     <AccountLayout>
-      <SEO title="My wishlist" />
-      <div className="orders-column">
+      <SEO title="My Orders" />
+      <OrdersColumn>
+        <h1>Orders</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
         <div className="current-orders">
-          <h1>Orders</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-          <h2>current orders</h2>
-          <OrdersItem />
+          <h2>Current orders</h2>
+          <ListPlain className="grid-item">
+            <li>
+              <OrdersItem />
+            </li>
+          </ListPlain>
         </div>
         <div className="previous-orders">
           <h2>Previous Orders</h2>
-          <OrdersItem />
+          <ListPlain className="grid-item">
+            <li>
+              <OrdersItem />
+            </li>
+            <li>
+              <OrdersItem />
+            </li>
+            <li>
+              <OrdersItem />
+            </li>
+            <li>
+              <OrdersItem />
+            </li>
+            <li>
+              <OrdersItem />
+            </li>
+          </ListPlain>
         </div>
-      </div>
+      </OrdersColumn>
     </AccountLayout>
   </Layout>
 )
