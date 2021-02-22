58493c145c311fe797140cad1364236e93325464
Karl Podger <karl.podger@primeordinal.com>
2021-02-22 22:23:07 +0000

    Tidy style rules

--- src/styles/formCheckboxContainer.tsx
+++ src/styles/formCheckboxContainer.tsx
@@ -1,60 +1,53 @@
 export const formCheckboxContainer = `
-
-  .field {
-
-  } 
-  
   .fancy-checkbox {
-      clip: rect(1px, 1px, 1px, 1px);
-      position: absolute !important;
-      height: 1px;
-      width: 1px;
-      overflow: hidden;
+    clip: rect(1px, 1px, 1px, 1px);
+    height: 1px;
+    overflow: hidden;
+    position: absolute !important;
+    width: 1px;
   }
   
   .fancy-checkbox-label {
-    display: block;
     cursor: pointer;
-    position: relative;
-    width: 100%;
+    display: block;
     font-size: 14px;
-    text-transform: capitalize;
     font-weight: normal;
-  
+    position: relative;
+    text-transform: capitalize;
+    width: 100%;
   }
   
   label.fancy-checkbox-label:before {
-      position: absolute;
-      display: block;
-      width: 18px;
-      height: 18px;
-      border: 1px solid #363139;
-      border-radius: 3px;
-      background: #FFF;
-      content: " ";
-      right: 0px;
-      top: 50%;
-      transform: translateY(-50%);
-
-  
-    }
+    background: #FFF;
+    border-radius: 3px;
+    border: 1px solid #363139;
+    content: " ";
+    display: block;
+    height: 18px;
+    position: absolute;
+    right: 0px;
+    top: 50%;
+    transform: translateY(-50%);
+    width: 18px;
+  }
   
   label.fancy-checkbox-label:after {
-    position: absolute;
-    top: 3px;
-    right: 2px;
-    width: 13px;
-    height: 7px;
-    content: " ";
-    transform: rotate(-53deg);
     border-bottom: 1px solid #363139;
     border-left: 1px solid #363139;
+    content: " ";
+    height: 7px;
     opacity: 0;
-    }
+    position: absolute;
+    right: 2px;
+    top: 3px;
+    transform: rotate(-53deg);
+    width: 13px;
+  }
   
   input:checked + label.fancy-checkbox-label:after {
     opacity:1;
   }
+
   input:checked + label.fancy-checkbox-label {
     font-weight: 600;
   }
