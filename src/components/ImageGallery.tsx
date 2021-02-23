1efbca89f7ea049cdcbb5ef8418db40ef4b9b160
Karl Podger <karl.podger@primeordinal.com>
2021-02-23 18:32:14 +0000

    Add custom UI icons to ImageGallery

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx"
 import React from "react"
 import ReactImageGallery from "react-image-gallery"
 import styled from "styled-components"
@@ -12,6 +13,10 @@ import {
   VariantProps,
 } from "styled-system"
 
+import { ReactComponent as Arrow } from "../images/Arrow.svg"
+import { ReactComponent as ZoomIn } from "../images/ZoomIn.svg"
+import { ReactComponent as Close } from "../images/Close.svg"
+
 import "./ImageGallery.css"
 
 export type ImageGalleryProps = LayoutProps &
@@ -23,10 +28,60 @@ export type ImageGalleryProps = LayoutProps &
 
 export const ImageGalleryStyled: React.FC<ImageGalleryProps> = styled(
   ReactImageGallery
-)`  
+)`
   ${compose(layout, position, space)}
 `
 
+// function renderCustomControls() {
+//   return <button type="button" className='image-gallery-custom-action' onClick={this._customAction.bind(this)} />
+// }
+
+const renderLeftNav = (onClick, disabled) => {
+  return (
+    <button
+      className={clsx("ui-button", "image-gallery-left-nav")}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      <Arrow className="image-gallery-svg" />
+    </button>
+  )
+}
+
+const renderRightNav = (onClick, disabled) => {
+  return (
+    <button
+      className={clsx("ui-button", "image-gallery-right-nav")}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      <Arrow className={clsx("image-gallery-svg", "ui", "zoom-in")} />
+    </button>
+  )
+}
+
+const renderFullscreenButton = (onClick, isFullscreen) => {
+  return (
+    <button
+      className={clsx(
+        "ui-button",
+        "image-gallery-fullscreen-button",
+        isFullscreen ? " active" : undefined
+      )}
+      onClick={onClick}
+      type="button"
+    >
+      <ZoomIn className={clsx("image-gallery-svg", "ui", "zoom-in")} />
+      <Close className={clsx("image-gallery-svg", "ui", "zoom-out")} />
+    </button>
+  )
+}
+
 export const ImageGallery: React.FC<ImageGalleryProps> = props => (
-  <ImageGalleryStyled {...props} />
+  <ImageGalleryStyled
+    renderLeftNav={renderLeftNav}
+    renderRightNav={renderRightNav}
+    renderFullscreenButton={renderFullscreenButton}
+    {...props}
+  />
 )
