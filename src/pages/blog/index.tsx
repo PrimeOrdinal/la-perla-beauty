cc963821e83682a5ea58b19508e178d054236d92
Khalid <kmnahari89@gmail.com>
2021-02-25 12:27:50 +0000

    Add breadcrumbs and imports to blog landing page

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,13 +1,29 @@
 import React from "react"
+import styled from "styled-components"
+import { themeGet } from "@styled-system/theme-get"
+import { mediaQueries } from "../../theme"
+
+import type { BigCommerceGql_Category } from "/../../graphql-types"
+import { PageProps } from "gatsby"
+
+import { Breadcrumb } from "../../components/Breadcrumb"
 import { BlogPostPreview } from "../../components/BlogPostPreview"
 
-const BlogLandingPage = () => {
+type PageContextCategory = PageContextTypeBreadcrumb & {
+  category: BigCommerceGql_Category
+}
+
+const BlogLandingPage: React.FC<PageProps<PageContextCategory>> = ({
+  pageContext,
+}) => {
+  const {
+    breadcrumb: { crumbs },
+  } = pageContext
   return (
-    <div>
-      <h1>Hello</h1>
-      <BlogPostPreview headingAbove />
-      <BlogPostPreview headingBelow />
-      <BlogPostPreview headingAbove />
+    <div className="container">
+      <h1>hey there</h1>
+      <Breadcrumb crumbs={crumbs} />
+      <BlogPostPreview headingAbove body />
     </div>
   )
 }
