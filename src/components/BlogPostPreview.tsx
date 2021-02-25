1a78b9cf052fd5cbabd720813152c32cb0f87b6b
Khalid <kmnahari89@gmail.com>
2021-02-25 18:11:21 +0000

    Adjust the image sizing on BlogPostPrev for test

--- src/components/BlogPostPreview.tsx
+++ src/components/BlogPostPreview.tsx
@@ -16,6 +16,7 @@ import {
 } from "styled-system"
 
 import { Link } from "./Button"
+import { mediaQueries } from "../theme"
 
 export type BlogPostPreviewProps = GridProps &
   LayoutProps &
@@ -26,25 +27,47 @@ export type BlogPostPreviewProps = GridProps &
     headingBelow: string
     tag: string
     body: string
+    link: string
   }
+
+const BlogPostPreviewStyled = styled.article`
+  img {
+    height: 250px;
+    width: 100%;
+    object-fit: cover;
+    ${mediaQueries.md} {
+      max-width: 100%;
+      height: 415px;
+    }
+  }
+  span {
+    display: block;
+    text-transform: uppercase;
+  }
+  a {
+    text-transform: uppercase;
+    font-weight: bold;
+  }
+`
 
 export const BlogPostPreview: React.FC<BlogPostPreviewProps> = ({
   headingAbove,
   headingBelow,
   tag,
   body,
+  link,
 }) => (
-  <article>
-    {headingAbove && <h1>Title above</h1>}
-    <img src="https://picsum.photos/405/540" alt="placeholder image" />
-    {tag && <span>be inspired</span>}
-    {headingBelow && <h1>Title below</h1>}
-    {body && (
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia ratione
-        voluptatum eaque et dolorum.
-      </p>
-    )}
-    <Link>Read more</Link>
-  </article>
+  <BlogPostPreviewStyled>
+    {headingAbove && <h2>{headingAbove}</h2>}
+    <img
+      src={`https://picsum.photos/905/740?${
+        Math.floor(Math.random() * 10) + 1
+      }`}
+      alt="placeholder image"
+    />
+    {tag && <span>{tag}</span>}
+    {headingBelow && <h2>{headingBelow}</h2>}
+    {body && <p>{body}</p>}
+    {link && <Link>{link}</Link>}
+  </BlogPostPreviewStyled>
 )
