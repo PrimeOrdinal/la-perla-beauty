c03da7b9aac04a890090c48512899669927999fa
Khalid <kmnahari89@gmail.com>
2021-03-01 13:56:19 +0000

    Create aspect-ratio attribute with custom values

---
+++ src/styles/aspectRatio.tsx
@@ -0,0 +1,54 @@
+export const aspectRatio = `
+[aspect-ratio] {
+    position: relative;
+    display: block;
+    margin: 0;
+    &::before {
+      content: "";
+      display: block;
+      width: 100%;
+    }
+    img {
+      height: 100%;
+      width: 100%;
+      object-fit: cover;
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+    }
+  }
+
+  [aspect-ratio="3/2"] {
+      &::before {
+        padding-top: calc(100% / (3/2));
+        aspect-ratio: 3 / 2;
+      }    
+  }
+
+  [aspect-ratio="16/9"] {
+      &::before {
+          padding-top: calc(100% / (16/9));
+          aspect-ratio: 16 / 9;
+      }
+  }
+  [aspect-ratio="1/1"]{
+      &::before {
+          padding-top: calc(100% /(1/1));
+          aspect-ratio: 1 / 1;
+      }
+  }
+  [aspect-ratio="3/4"] {
+      &::before {
+          padding-top: calc(100% / (3/4));
+          aspect-ratio: 3 / 4
+      }
+  }
+  [aspect-ratio="615/413"] {
+    &::before {
+        padding-top: calc(100% / (615/413));
+        aspect-ratio: 615 / 413;
+    }
+}
+`
