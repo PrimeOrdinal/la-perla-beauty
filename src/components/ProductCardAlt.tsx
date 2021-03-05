c8f0b81fc27f4a3ffc9c3fc5f5861d6fa73c9a07
Khalid <kmnahari89@gmail.com>
2021-03-05 17:55:14 +0000

    Add a white bg to the ProductCardAlt

---
+++ src/components/ProductCardAlt.tsx
@@ -0,0 +1,101 @@
+import React from "react"
+import styled from "styled-components"
+
+import WishlistAbsent from "../images/WishlistAbsent.svg"
+import Share from "../images/Share.svg"
+import { Button } from "./Button"
+
+const ProductCardAltStyled = styled.article`
+  background: white;
+  border-radius: 20px;
+  display: inline-grid;
+  padding: 1rem;
+  margin-top: 5rem;
+  gap: 1rem;
+  .productImage {
+    margin-top: -5rem;
+  }
+  .productType {
+    display: flex;
+    justify-content: space-between;
+    span {
+      text-transform: uppercase;
+      font-size: 12px;
+    }
+    .productIcons {
+      display: grid;
+      grid-auto-flow: column;
+      gap: 1rem;
+      img {
+        width: 21px;
+        height: 18px;
+      }
+    }
+  }
+  h3 {
+    font-size: 18px;
+    margin: 0;
+    padding-block-end: 1rem;
+    border-bottom: 1px solid #aeaeae;
+  }
+  .productInfo {
+    display: grid;
+    grid-auto-flow: column;
+    justify-content: space-between;
+    span {
+      font-weight: bold;
+      font-size: 14px;
+    }
+    .price {
+      font-family: "Tiempos";
+      font-weight: 300;
+      font-size: 16px;
+    }
+  }
+`
+
+export type ProductCardAltProps = {
+  image: {
+    src: string
+    alt: string
+  }
+  category: string
+  name: string
+  size?: string
+  price: string
+  colour?: string
+}
+
+export const ProductCardAlt: React.FC<ProductCardAltProps> = ({
+  image,
+  category,
+  name,
+  size,
+  price,
+  colour,
+}) => {
+  return (
+    <ProductCardAltStyled>
+      {image && (
+        <img className="productImage" src={image.src} alt={image.alt} />
+      )}
+      <div className="productType">
+        {category && <span>{category}</span>}
+        <div className="productIcons">
+          <img className="img-bl" src={Share} alt="share" />
+          <img className="img-bl" src={WishlistAbsent} alt="like" />
+        </div>
+      </div>
+      <h3>{name}</h3>
+      <div className="productInfo">
+        {(size && <span>SIZE | {size}ml</span>) ||
+          (colour && <span>COLOUR | {colour}</span>)}
+        {price && <span className="price">£{price}</span>}
+      </div>
+      <Button variant="primary">Add to basket</Button>
+      <Button variant="tertiary">view product</Button>
+    </ProductCardAltStyled>
+  )
+}
+
+export default ProductCardAlt
