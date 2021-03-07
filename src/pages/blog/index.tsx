b54e3c50293229e6f1b3ceda3cbde298ce1da49f
Khalid <kmnahari89@gmail.com>
2021-03-07 21:35:08 +0000

    Update markup on blog landing page

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,4 +1,7 @@
-import type { BigCommerceGql_Category, BlogLandingPageQuery } from "/../../graphql-types"
+import type {
+  BigCommerceGql_Category,
+  BlogLandingPageQuery,
+} from "/../../graphql-types"
 
 import { themeGet } from "@styled-system/theme-get"
 import { PageProps } from "gatsby"
@@ -48,89 +51,113 @@ const imageprops = {
 }
 
 const MainStyled = styled.main`
-  h1 {
-    margin-block-start: 0;
-    font-size: ${themeGet("fontSizes.6")}px;
-    ${mediaQueries.md} {
-      font-size: ${themeGet("fontSizes.7")}px;
+  overflow-x: hidden;
+  ${mediaQueries.md} {
+    overflow-x: unset;
+  }
+  .first {
+    h1 {
+      font-size: 21px;
+      ${mediaQueries.md} {
+        font-size: 32px;
+      }
     }
-  }
-  h2 {
-    ${mediaQueries.md} {
-      font-size: ${"fontSizes.6"}px;
+    p {
+      ${mediaQueries.md} {
+        font-size: 14px;
+      }
     }
-  }
-  p {
-    max-width: 90ch;
-  }
-
-  .second-section {
-    padding-block-start: ${themeGet("space.10")}px;
-    padding-block-end: ${themeGet("space.12")}px;
-  }
-  .first-section {
-    padding-block-start: ${themeGet("space.10")}px;
-  }
-  .fourth-section {
-    background: #bfcea5;
-  }
-  .fifth-section {
-    ul {
+    &__grid {
       display: grid;
-      grid-template-columns: 1fr 1fr;
+      grid-auto-flow: row;
       gap: 1rem;
       ${mediaQueries.md} {
-        grid-template-columns: repeat(4, 1fr);
+        grid-template-columns: repeat(2, 1fr);
+      }
+      article {
+        figure {
+          margin-left: -10%;
+          margin-right: -10%;
+          ${mediaQueries.md} {
+            margin-left: unset;
+            margin-right: unset;
+          }
+        }
       }
     }
   }
-`
-
-const FeaturedGrid = styled(ListPlain)`
-  display: grid;
-  gap: 34px;
-  grid-auto-flow: row;
-  ${mediaQueries.md} {
-    grid-template-columns: 1fr 1fr;
+  .second {
+    h2 {
+      font-size: 21px;
+      ${mediaQueries.md} {
+        font-size: 32px;
+      }
+    }
+    p {
+      ${mediaQueries.md} {
+        font-size: 14px;
+      }
+    }
+    &__grid {
+      ${mediaQueries.md} {
+        display: grid;
+        grid-auto-flow: column;
+        gap: 1rem;
+      }
+      .col1,
+      .col2 {
+        display: grid;
+        grid-auto-flow: column;
+        gap: 1rem;
+        min-height: 375px;
+        article:last-child {
+          align-self: flex-end;
+        }
+      }
+    }
   }
-  border-bottom: 1px ${themeGet("border.color")} solid;
-  padding-block-end: ${themeGet("space.12")}px;
-`
-const RecommendedGrid = styled(ListPlain)`
-  display: grid;
-  grid-template-columns: auto auto;
-  gap: 15px;
-  min-height: 770px;
-  ${mediaQueries.md} {
-    grid-auto-flow: column;
-    gap: 40px;
-    min-height: 600px;
+  .third {
+    article {
+      figure {
+        margin-left: -10%;
+        margin-right: -10%;
+        ${mediaQueries.md} {
+          margin-left: unset;
+          margin-right: unset;
+        }
+      }
+    }
   }
-  li {
-    &:nth-child(even) {
-      align-self: flex-end;
+  .fourth {
+    background: #bfcea5;
+    padding-block-start: 1rem;
+    padding-block-end: 1rem;
+    &__grid {
+      display: grid;
+      grid-auto-flow: row;
+      gap: 1rem;
+      place-items: center;
+      ${mediaQueries.sm} {
+        grid-template-columns: auto auto;
+      }
+      ${mediaQueries.md} {
+        grid-auto-flow: column;
+      }
+    }
+  }
+  .fifth {
+    &__grid {
+      display: grid;
+      grid-template-columns: repeat(4, 200px);
+      gap: 2rem;
+      overflow-x: scroll;
     }
   }
 `
 
-const ProductCardSection = styled(ListPlain)`
-  display: grid;
-  grid-auto-flow: row;
-  gap: 1rem;
-  place-content: center;
-  ${mediaQueries.md} {
-    grid-template-columns: repeat(2, 1fr);
-    justify-items: center;
-  }
-  ${mediaQueries.lg} {
-    grid-template-columns: repeat(4, 1fr);
-    justify-items: unset;
-  }
-`
-
-const BlogLandingPage: React.FC<PageProps<BlogLandingPageQuery, PageContextCategory>> = ({
-  pageContext,
-}) => {
+const BlogLandingPage: React.FC<
+  PageProps<BlogLandingPageQuery, PageContextCategory>
+> = ({ pageContext }) => {
   const {
     breadcrumb: { crumbs },
   } = pageContext
@@ -142,189 +169,159 @@ const BlogLandingPage: React.FC<PageProps<BlogLandingPageQuery, PageContextCateg
       <ImageGallery {...imageprops} className="img-bl" />
       <MenuSubCategory>View All</MenuSubCategory>
       <MainStyled>
-        <section className="container first-section">
-          <h1>Top stories</h1>
+        <section className="first container">
+          <h1>Top Stories</h1>
           <p>
             Vestibulum interdum metus at mi volutpat, in pulvinar diam suscipit.
             Vivamus at tortor ullamcorper, aliquam lorem nec, varius arcu.
           </p>
-          <div>
-            <FeaturedGrid>
-              <li>
-                <BlogPostPreview
-                  tag="be inspired"
-                  headingBelow="The exquisite sense of mere tranquil existence"
-                  link="explore & shop"
-                  aspectRatio="615/413"
-                />
-              </li>
-              <li>
-                <BlogPostPreview
-                  tag="featured"
-                  headingBelow="Never was there a greater artist than there is now"
-                  link="explore & shop"
-                  aspectRatio="615/413"
-                />
-              </li>
-            </FeaturedGrid>
+          <div className="first__grid">
+            <BlogPostPreview
+              headingBelow="The exquisite sense of mere tranquil existence"
+              tag="Be inspired"
+              link="explore and shop"
+              aspectRatio="615/413"
+            />
+            <BlogPostPreview
+              headingBelow="Never was there a greater artist than there is now"
+              tag="featured"
+              link="explore and shop"
+              aspectRatio="615/413"
+            />
           </div>
+          <hr />
         </section>
-        <section className="container second-section">
-          <h1>Recommended</h1>
+        <section className="second container">
+          <h2>Recommended</h2>
           <p>
             Nullam sit amet laoreet urna. Donec quis erat feugiat, ultrices eros
             nec, tincidunt nisl. Sed bibendum eu sapien eleifend feugiat. Cras
             efficitur erat aliquam.
           </p>
-          <div>
-            <RecommendedGrid>
-              <li>
-                <BlogPostPreview
-                  headingAbove="Etiam aliquet vist metus"
-                  body="Luxuary with integrity"
-                  link="read more"
-                  aspectRatio="3/4"
-                />
-              </li>
-              <li>
-                <BlogPostPreview
-                  headingAbove="Etiam aliquet vist metus"
-                  body="Luxuary with integrity"
-                  aspectRatio="3/4"
-                  link="read more"
-                />
-              </li>
-              <li>
-                <BlogPostPreview
-                  headingAbove="Etiam aliquet vist metus"
-                  body="Luxuary with integrity"
-                  aspectRatio="3/4"
-                  link="read more"
-                />
-              </li>
-              <li>
-                <BlogPostPreview
-                  headingAbove="Etiam aliquet vist metus"
-                  body="Luxuary with integrity"
-                  aspectRatio="3/4"
-                  link="read more"
-                />
-              </li>
-            </RecommendedGrid>
+          <div className="second__grid">
+            <div className="col1">
+              <BlogPostPreview
+                headingBelow="Etiam aliquet vist metus"
+                body="Luxury with integrity​"
+                link="read more"
+                aspectRatio="3/4"
+              />
+              <BlogPostPreview
+                headingBelow="Etiam aliquet vist metus"
+                body="Luxury with integrity​"
+                link="read more"
+                aspectRatio="3/4"
+              />
+            </div>
+            <div className="col2">
+              <BlogPostPreview
+                headingBelow="Etiam aliquet vist metus"
+                body="Luxury with integrity​"
+                link="read more"
+                aspectRatio="3/4"
+              />
+              <BlogPostPreview
+                headingBelow="Etiam aliquet vist metus"
+                body="Luxury with integrity​"
+                link="read more"
+                aspectRatio="3/4"
+              />
+            </div>
           </div>
         </section>
-        <section className="container third-section">
+        <section className="third container">
           <BlogPostPreview
+            headingBelow="The buzz of the little world"
             tag="videos"
-            headingBelow="The buzz of the little wolrd"
             link="learn more"
             aspectRatio="615/413"
           />
         </section>
-        <section className="container fourth-section">
-          <ProductCardSection>
-            <li>
-              <ProductCardAlt
-                image={{
-                  src: `https://picsum.photos/277/277?${
-                    Math.floor(Math.random() * 10) + 1
-                  }`,
-                  alt: "placeholder",
-                }}
-                category="EAU DE PARFUM"
-                name="The Scent of La Perla"
-                price="123"
-              />
-            </li>
-            <li>
-              <ProductCardAlt
-                image={{
-                  src: `https://picsum.photos/277/277?${
-                    Math.floor(Math.random() * 10) + 1
-                  }`,
-                  alt: "placeholder",
-                }}
-                category="EAU DE PARFUM"
-                name="The Scent of La Perla"
-                price="123"
-              />
-            </li>
-            <li>
-              <ProductCardAlt
-                image={{
-                  src: `https://picsum.photos/277/277?${
-                    Math.floor(Math.random() * 10) + 1
-                  }`,
-                  alt: "placeholder",
-                }}
-                category="EAU DE PARFUM"
-                name="The Scent of La Perla"
-                price="123"
-              />
-            </li>
-            <li>
-              <ProductCardAlt
-                image={{
-                  src: `https://picsum.photos/277/277?${
-                    Math.floor(Math.random() * 10) + 1
-                  }`,
-                  alt: "placeholder",
-                }}
-                category="EAU DE PARFUM"
-                name="The Scent of La Perla"
-                price="123"
-              />
-            </li>
-          </ProductCardSection>
+        <section className="fourth">
+          <div className="fourth__grid">
+            <ProductCardAlt
+              image={{
+                src: `https://picsum.photos/231/231?${
+                  Math.floor(Math.random() * 10) + 1
+                }`,
+                alt: "something",
+              }}
+              size="120"
+              name="The Scent of La Perla"
+              price="120"
+              category="Eau de parfum"
+            />
+            <ProductCardAlt
+              image={{
+                src: `https://picsum.photos/231/231?${
+                  Math.floor(Math.random() * 10) + 1
+                }`,
+                alt: "something",
+              }}
+              size="120"
+              name="The Scent of La Perla"
+              price="120"
+              category="Eau de parfum"
+            />
+            <ProductCardAlt
+              image={{
+                src: `https://picsum.photos/231/231?${
+                  Math.floor(Math.random() * 10) + 1
+                }`,
+                alt: "something",
+              }}
+              size="120"
+              name="The Scent of La Perla"
+              price="120"
+              category="Eau de parfum"
+            />
+            <ProductCardAlt
+              image={{
+                src: `https://picsum.photos/231/231?${
+                  Math.floor(Math.random() * 10) + 1
+                }`,
+                alt: "something",
+              }}
+              size="120"
+              name="The Scent of La Perla"
+              price="120"
+              category="Eau de parfum"
+            />
+          </div>
         </section>
-        <section className="container fifth-section">
-          <h1>Don't miss</h1>
-          <hr />
+        <section className="fifth container">
+          <h2>Don't Miss</h2>
           <p>
             Etiam aliquet metus mauris, tempus odio feugiat non. Praesent eu
-            magna id hendrerit bibendum. Aenean eget aliquet nisl. Pellentesque.
+            magna id hendrerit bibendum. Aenean eget aliquet nisl. Pellentesque.{" "}
           </p>
-          <BlogPostPreview
-            headingBelow="The breath of that universal love"
-            link="explore & shop"
-            aspectRatio="615/413"
-            body="Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae. Vivamus a felis et odio viverra dignissim."
-          />
-          <ListPlain>
-            <li>
-              <BlogPostPreview
-                headingAbove="The breath of that universal love"
-                link="explore & shop"
-                aspectRatio="3/4"
-                body="Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae. Vivamus a felis et odio viverra dignissim."
-              />
-            </li>
-
-            <li>
-              <BlogPostPreview
-                headingBelow="The breath of that universal love"
-                link="explore & shop"
-                aspectRatio="3/4"
-                body="Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae. Vivamus a felis et odio viverra dignissim."
-              />
-            </li>
-            <li>
-              <BlogPostPreview
-                headingAbove="The breath of that universal love"
-                link="explore & shop"
-                aspectRatio="3/4"
-                body="Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae. Vivamus a felis et odio viverra dignissim."
-              />
-            </li>
-            <li>
-              <BlogPostPreview
-                headingBelow="The breath of that universal love"
-                link="explore & shop"
-                aspectRatio="3/4"
-                body="Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae. Vivamus a felis et odio viverra dignissim."
-              />
-            </li>
-          </ListPlain>
+          <BlogPostPreview />
+          <div className="fifth__grid">
+            <BlogPostPreview
+              headingAbove="The key to more success is to get a massage"
+              body="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+              link="read more"
+              aspectRatio="3/4"
+            />
+            <BlogPostPreview
+              headingBelow="Don’t panic, when it gets crazy and rough"
+              body="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+              link="read more"
+              aspectRatio="3/4"
+            />
+            <BlogPostPreview
+              headingAbove="The key to more success is to get a massage"
+              body="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+              link="read more"
+              aspectRatio="3/4"
+            />
+            <BlogPostPreview
+              headingBelow="Don’t panic, when it gets crazy and rough"
+              body="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+              link="read more"
+              aspectRatio="3/4"
+            />
+          </div>
         </section>
       </MainStyled>
     </Layout>
