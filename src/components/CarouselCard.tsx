d9acbf059cde1fdeda466ed0decf20f80f138d3d
Khalid <kmnahari89@gmail.com>
2021-03-07 16:23:57 +0000

    CarouselCard for blog pages with date

---
+++ src/components/CarouselCard.tsx
@@ -0,0 +1,68 @@
+import React from "react"
+import styled from "styled-components"
+
+const CarouselCardStyled = styled.div`
+  display: inline-grid;
+  grid-auto-flow: row;
+  background: #98825a;
+  border-radius: 18px;
+  margin: 4rem;
+  figure {
+    margin: 0;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      border-radius: 18px 18px 0 0;
+    }
+  }
+  div {
+    padding: 20px;
+    text-align: center;
+    display: grid;
+    grid-auto-flow: row;
+    gap: 16px;
+    padding-block-start: 32px;
+    padding-block-end: 32px;
+    time {
+      font-size: 13px;
+    }
+    h2 {
+      margin: 0;
+    }
+    p {
+      margin: 0;
+    }
+  }
+`
+
+export type CarouselCardProps = {
+  image: {
+    src: string
+    alt: string
+  }
+  date: string
+  heading: string
+  body: string
+}
+
+export const CarouselCard: React.FC<CarouselCardProps> = ({
+  image,
+  date,
+  heading,
+  body,
+}) => {
+  return (
+    <CarouselCardStyled>
+      <figure>
+        <img className="img-bl" src={image.src} alt={image.alt} />
+      </figure>
+      <div>
+        <time>{date}</time>
+        <h2>{heading}</h2>
+        <p>{body}</p>
+      </div>
+    </CarouselCardStyled>
+  )
+}
