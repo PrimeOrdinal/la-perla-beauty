4c62df53d7b3cf81ea39dd3eddcc4379097b04a2
Karl Podger <karl.podger@primeordinal.com>
2021-03-12 13:20:51 +0000

    Fix story implementation for ItemAvailability

--- stories/ItemAvailability.stories.tsx
+++ stories/ItemAvailability.stories.tsx
@@ -1,30 +1,41 @@
 import React from "react"
 import { Story, Meta } from "@storybook/react"
 
-import { ItemAvailability, ItemAvailabilityProps } from "../src/components/ItemAvailability"
+import {
+  ItemAvailability,
+  ItemAvailabilityProps,
+} from "../src/components/ItemAvailability"
 
 export default {
   title: "ItemAvailability",
   component: ItemAvailability,
   argTypes: {
     backgroundColor: { control: "color" },
-    variant: {
+    availability: {
       control: {
         type: "inline-radio",
-        options: ["primary", "secondary"],
+        options: [
+          "InStock",
+          "LimitedAvailability",
+          "OnlineOnly",
+          "OutOfStock",
+          "PreOrder",
+          "SoldOut",
+        ],
       },
     },
   },
 } as Meta
 
-const Template: Story<ItemAvailabilityProps> = args => <ItemAvailability {...args} />
+const Template: Story<ItemAvailabilityProps> = args => (
+  <ItemAvailability {...args} />
+)
 
 export const Example = Template.bind({})
 Example.args = {
   children: "Pre-order",
-  variant: "primary",
-  // width: { _: 1, sm: 1/2, md: 1/2, lg: 1/6 }
-  maxWidth: 256
+  availability: "InStock",
+  maxWidth: 256,
 }
 Example.argTypes = {
   onClick: { action: "ItemAvailability clicked" },
