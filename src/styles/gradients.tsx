fa2bb234a78e9ff62ee657ae43ba920a75647d8a
Karl Podger <karl.podger@primeordinal.com>
2021-03-14 12:20:13 +0000

    Refine image-overlay-gradient style

--- src/styles/gradients.tsx
+++ src/styles/gradients.tsx
@@ -1,6 +1,5 @@
 export const gradients = `
     .image-overlay-gradient {
-        background: linear-gradient(180deg, rgba(102,95,107,0) 0%, #363139 100%);
-        opacity: 0.85;
+        background: linear-gradient(180deg, rgba(102,95,107,0) 0%, #363139 85%);
     }
 `
