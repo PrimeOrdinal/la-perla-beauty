d37d22e93200b5c3c8495aaf16007bd0fae1b8ac
Karl Podger <karl.podger@primeordinal.com>
2021-03-14 17:07:57 +0000

    WIP refactor of ProductCardAlt prop sstructure

--- src/components/ProductCardAlt.tsx
+++ src/components/ProductCardAlt.tsx
@@ -1,11 +1,33 @@
+import type { Offer, Product } from "schema-dts"
+
+import type { Colour as ColourProp } from "../../types/components"
+
 import { themeGet } from "@styled-system/theme-get"
 import React from "react"
 import styled from "styled-components"
+import {
+  color,
+  compose,
+  flexbox,
+  grid,
+  layout,
+  position,
+  space,
+  ColorProps,
+  FlexboxProps,
+  GridProps,
+  LayoutProps,
+  PositionProps,
+  SpaceProps,
+  VariantProps,
+} from "styled-system"
 
 import Share from "../../static/icons//Share.svg"
 import WishlistAbsent from "../../static/icons//WishlistAbsent.svg"
 
-import { Button } from "./Button"
+import { Link } from "./Button"
+import { Price } from "./Price"
+import { QuickWishlist } from "./QuickWishlist"
 
 const ProductCardAltStyled = styled.article`
   background-color: ${themeGet("colors.background")};
@@ -16,7 +38,7 @@ const ProductCardAltStyled = styled.article`
   margin-block-start: 5rem;
   padding: 34px;
 
-  .productImage {
+  .image-container {
     margin-block-start: -5rem;
   }
 
@@ -48,7 +70,7 @@ const ProductCardAltStyled = styled.article`
     padding-block-end: 1rem;
   }
 
-  .productInfo {
+  .product-information {
     display: grid;
     grid-auto-flow: column;
     justify-content: space-between;
@@ -64,48 +86,83 @@ const ProductCardAltStyled = styled.article`
       font-size: 16px;
     }
   }
+
+  ${compose(color, flexbox, grid, layout, position, space)}
 `
 
-export type ProductCardAltProps = {
-  image: {
-    src: string
-    alt: string
+export type ProductCardAltProps = ColorProps &
+  FlexboxProps &
+  GridProps &
+  LayoutProps &
+  PositionProps &
+  SpaceProps &
+  VariantProps & {
+    colour?: ColourProp
+    product: Product
+    variantType: "color" | "size"
   }
-  category: string
-  name: string
-  size?: string
-  price: string
-  colour?: string
-}
 
 export const ProductCardAlt: React.FC<ProductCardAltProps> = ({
-  image,
-  category,
-  name,
-  size,
-  price,
-  colour,
+  product,
+  ...props
 }) => {
+  const offer = product?.offers as Offer
+
+  const thumbnail = product?.image?.[0]
+
+  const image = thumbnail ? (
+    <img
+      alt={product?.name as string}
+      itemProp="image"
+      src={thumbnail?.contentUrl}
+    />
+  ) : undefined
+
   return (
     <ProductCardAltStyled>
-      {image && (
-        <img className="productImage" src={image.src} alt={image.alt} />
-      )}
+      {(image && product?.url && (
+        <Link
+          className="image-container"
+          to={product?.url as string}
+          title={product?.title as string}
+        >
+          {image}
+        </Link>
+      )) ||
+        image}
       <div className="productType">
-        {category && <span>{category}</span>}
+        {props.category && <span>{props.category}</span>}
         <div className="productIcons">
           <img className="img-bl" src={Share} alt="share" />
           <img className="img-bl" src={WishlistAbsent} alt="like" />
         </div>
       </div>
-      <h3>{name}</h3>
-      <div className="productInfo">
-        {(size && <span>SIZE | {size}ml</span>) ||
-          (colour && <span>COLOUR | {colour}</span>)}
-        {price && <span className="price">£{price}</span>}
+      <h3>{props.name}</h3>
+      <div className="product-information">
+        {(props.variantType === "size" && (
+          <div className="product-information">
+            <span className="label">Size</span> |{" "}
+            <span className="value">{props.product?.size}</span>
+          </div>
+        )) ||
+          (props.variantType === "color" && (
+            <div className="product-information">
+              <span className="label">Colour</span> |{" "}
+              <span className="value">{props.product?.color}</span>
+            </div>
+          ))}
+        {offer && <Price display={{ _: "none", md: "block" }} offer={offer} />}
       </div>
-      <Button variant="primary">Add to basket</Button>
-      <Button variant="tertiary">View product</Button>
+      <div className="product-actions">
+        <QuickWishlist product={product} />
+        <Link
+          title={product?.title as string}
+          to={product?.url as string}
+          variant="tertiary"
+        >
+          View product
+        </Link>
+      </div>
     </ProductCardAltStyled>
   )
 }
