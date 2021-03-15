23c3386ea0031853a41ef638f22bca3b62ec0a47
Karl Podger <karl.podger@primeordinal.com>
2021-03-15 21:45:53 +0000

    Create pages for blog articles

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,7 +6,7 @@
 
 const path = require("path")
 
-const stripSlashes = (text) => text.replace(/([^:]\/)\/+/g, "$1")
+const stripSlashes = text => text.replace(/([^:]\/)\/+/g, "$1")
 
 // exports.onCreatePage = ({ page, actions }) => {
 //   const { createPage, deletePage } = actions
@@ -20,61 +20,75 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
 
   // Query for markdown nodes to use in creating pages.
   const result = await graphql(`
-      query AllProducts {
-        allContentstackPage {
-          edges {
-            node {
-              id
-              locale
-              publish_details {
-                time
-                user
-                environment
-              }
-              template
-              title
+    query AllProducts {
+      allContentstackArticle {
+        edges {
+          node {
+            id
+            locale
+            publish_details {
+              time
+              user
+              environment
+            }
+            title
+            url
+          }
+        }
+      }
+      allContentstackPage {
+        edges {
+          node {
+            id
+            locale
+            publish_details {
+              time
+              user
+              environment
+            }
+            template
+            title
+            url
+          }
+        }
+      }
+      allBigCommerceProducts {
+        edges {
+          node {
+            bigcommerce_id
+            custom_url {
               url
             }
           }
         }
-        allBigCommerceProducts {
-          edges {
-            node {
-              bigcommerce_id
-              custom_url {
-                url
-              }
+      }
+      allBigCommerceCategories {
+        edges {
+          node {
+            bigcommerce_id
+            custom_url {
+              url
             }
+            description
+            name
           }
         }
-        allBigCommerceCategories {
-          edges {
-            node {
-              bigcommerce_id
-              custom_url {
-                url
-              }
-              description
-              name
-            }
-          }
-        }
-        bigCommerceGQL {
-          site {
-            settings {
-              status
-              storeHash
-              storeName
-              url {
-                cdnUrl
-                vanityUrl
-              }
+      }
+      bigCommerceGQL {
+        site {
+          settings {
+            status
+            storeHash
+            storeName
+            url {
+              cdnUrl
+              vanityUrl
             }
           }
         }
       }
-    `
-  )
+    }
+  `)
 
   // Handle errors
   if (result.errors) {
@@ -98,8 +112,24 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
     })
   })
 
+  // Create pages for blog articles.
+  result.data.allContentstackArticle.edges.forEach(({ node }) => {
+    const pagePath = node.url
+
+    createPage({
+      path: pagePath,
+      component: path.resolve(`src/templates/general.tsx`),
+      // In your template's graphql query, you can use pagePath
+      // as a GraphQL variable to query for data from the API.
+      context: {
+        contentstackId: node.id,
+        page: { breadcrumb: true, ...node },
+      },
+    })
+  })
+
   // Create pages for categories.
-  const categoryTemplate = path.resolve(`src/pages/category.tsx`)
+  const categoryTemplate = path.resolve(`src/templates/category.tsx`)
 
   result.data.allBigCommerceCategories.edges.forEach(({ node }) => {
     const pagePath = node.custom_url.url
@@ -111,14 +141,14 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
       // as a GraphQL variable to query for data from the API.
       context: {
         category: node,
-        id: node.bigcommerce_id
+        id: node.bigcommerce_id,
       },
     })
   })
 
   // Create pages for products.
   const productTemplate = path.resolve(`src/templates/product.tsx`)
-  
+
   result.data.allBigCommerceProducts.edges.forEach(({ node }) => {
     const pagePath = node.custom_url.url
 
@@ -126,7 +156,7 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
       path: pagePath,
       component: productTemplate,
       context: {
-        id: node.bigcommerce_id
+        id: node.bigcommerce_id,
       },
     })
   })
@@ -137,7 +167,7 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
 //   const typeDefs = `
 //     type Contentstack_sectionsModular_blocks implements Node {
 //       menu {
-          
+
 //       }
 //     }
 //   `
