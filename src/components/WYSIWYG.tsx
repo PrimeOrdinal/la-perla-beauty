5d0eebd5126744c71ede51fc1d85ecd5f66d5ee0
Karl Podger <karl.podger@primeordinal.com>
2021-03-19 10:55:41 +0000

    Set WYSIWYG header sizes

--- src/components/WYSIWYG.tsx
+++ src/components/WYSIWYG.tsx
@@ -1,3 +1,4 @@
+import { themeGet } from "@styled-system/theme-get"
 import React from "react"
 import styled from "styled-components"
 import {
@@ -30,6 +31,39 @@ export type WYSIWYGProps = ColorProps &
 
 export const WYSIWYGStyled: React.FC<WYSIWYGProps> = styled.section`
   max-width: ${props => props.width === "compact" ? "50%" : "100%"};
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-family: ${themeGet("fontFamily", "Tiempos")};
+  }
+
+  h1 {
+    font-size: var(--font-size-heading-1, 32px);
+  }
+
+  h2 {
+    font-size: var(--font-size-heading-2, 24px);
+  }
+
+  h3 {
+    font-size: var(--font-size-heading-3, 18px);
+  }
+
+  h4 {
+    font-size: var(--font-size-heading-4, 16px);
+  }
+
+  h5 {
+    font-size: var(--font-size-heading-5, 14px);
+  }
+
+  h6 {
+    font-size: var(--font-size-heading-6, 12px);
+  }
 
   ${compose(color, flexbox, grid, layout, position, space)}
 `
--- src/styles/headings.tsx
+++ src/styles/headings.tsx
@@ -9,28 +9,4 @@ export const headings = `
         font-weight: lighter;
         line-height: 1;
     }
-
-    .heading1 {
-        font-size: var(--font-size-heading-1, 16px);
-        letter-spacing: 0.5px;
-        line-height: 30px;
-    }
-
-    .heading2 {
-        font-size: var(--font-size-heading-2, 14px);
-        letter-spacing: 0.7px;
-        line-height: 15px;
-    }
-
-    .heading3 {
-        font-size: var(--font-size-heading-3, 13px);
-        letter-spacing: 0.7px;
-        line-height: 15px;
-    }
-
-    .heading4 {
-        font-size: var(--font-size-heading-4, 12px);
-        letter-spacing: 0.7px;
-        line-height: 15px;
-    }
 `
