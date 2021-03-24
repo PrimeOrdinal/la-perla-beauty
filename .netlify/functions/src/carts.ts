c998261584671c85ced006ba60274542a467c335
Karl Podger <karl.podger@primeordinal.com>
2021-03-24 22:56:18 +0000

    Refactor carts functions

--- .netlify/functions/src/carts.ts
+++ .netlify/functions/src/carts.ts
@@ -6,7 +6,13 @@ import fetch from "node-fetch"
 // https://github.com/DavidWells/netlify-functions-workshop/blob/master/lessons-code-complete/use-cases/1-rest-api/functions/api.js
 
 export const handler: Handler = async (event: APIGatewayEvent) => {
-  const { BIGCOMMERCE_ACCESS_TOKEN, BIGCOMMERCE_CLIENT_ID, BIGCOMMERCE_CLIENT_SECRET, BIGCOMMERCE_STORE_HASH, GATSBY_SITE_URL } = process.env
+  const {
+    BIGCOMMERCE_ACCESS_TOKEN,
+    BIGCOMMERCE_CLIENT_ID,
+    BIGCOMMERCE_CLIENT_SECRET,
+    BIGCOMMERCE_STORE_HASH,
+    GATSBY_SITE_URL,
+  } = process.env
 
   if (
     !BIGCOMMERCE_STORE_HASH ||
@@ -21,29 +27,29 @@ export const handler: Handler = async (event: APIGatewayEvent) => {
     }
   }
 
-  // event.httpMethod === "POST"
-  const path = event.path
-  // const { msg } = event.queryStringParameters
-  console.log(path)
+  const apiURL = `https://api.bigcommerce.com/stores/${BIGCOMMERCE_STORE_HASH}/v3/`
 
-  const apiURL = `https://api.bigcommerce.com/stores/${BIGCOMMERCE_STORE_HASH}/v3`
+  const headers = {
+    "X-Auth-Client": BIGCOMMERCE_CLIENT_ID,
+    "X-Auth-Token": BIGCOMMERCE_ACCESS_TOKEN,
+    "X-Client-Name": "gatsby-bigcommerce-netlify-cms-starter",
+    "X-Client-Type": "Gatsby",
+    "X-Plugin-Version": "1.0.0",
+    Accept: "application/json",
+  }
 
-  try {
-    if(!event.headers.cookie) {
-      return
-    }
+  const options = {
+    credentials: "include",
+    headers,
+    method: event.httpMethod
+  }
 
-    const cookies = cookie.parse(event.headers.cookie)
+  const createCart = async (event) => {
+    console.info("createCart")
 
-    const hasCartIdCookie = cookies.hasOwnProperty("cartId")
+    const path = "carts"
 
-    console.assert(hasCartIdCookie, cookies)
-
-    const path = hasCartIdCookie
-      ? `carts/${cookies.cartId}`
-      : `carts`
-
-    const url = new URL(`/${path}`, `${apiURL}`)
+    const url = new URL(path, `${apiURL}`)
 
     url.searchParams.append("include", "redirect_urls")
 
@@ -51,55 +57,126 @@ export const handler: Handler = async (event: APIGatewayEvent) => {
       line_items: [
         {
           quantity: 1,
-          product_id: 104
-        }
-      ]
+          product_id: 145,
+        },
+      ],
     }
 
-    const response = await fetch(url, {
-      body: JSON.stringify(data),
-      headers: {
-        "X-Auth-Client": BIGCOMMERCE_CLIENT_ID,
-        "X-Auth-Token": BIGCOMMERCE_ACCESS_TOKEN,
-        "X-Client-Name": "gatsby-bigcommerce-netlify-cms-starter",
-        "X-Client-Type": "Gatsby",
-        "X-Plugin-Version": "1.0.0",
-        Accept: "application/json",
-      },
-      method: hasCartIdCookie ? "get" : "post"
-    })
-
-    let cookieHeader = cookie.serialize("cartId", "", { maxAge: -1 })
+    const response = await fetch(url, {...options, body: JSON.stringify(data), method: "POST"})
 
     if (response.ok) {
-      const data = await response.json()
-  
+      const { data } = await response.json()
+      console.log("data", data)
+
       const { id } = data
 
-      cookieHeader = cookie.serialize("cartId", id, {
+      const cookieHeader = cookie.serialize("cartId", id, {
         maxAge: 60 * 60 * 24 * 28, // 4 weeks
       })
 
       return {
         body: JSON.stringify(data),
-        headers: {
-          "Access-Control-Allow-Headers": "Content-Type, Accept",
-          "Access-Control-Allow-Credentials": "true",
-          "Access-Control-Allow-Origin": GATSBY_SITE_URL,
-          "Access-Control-Allow-Methods": "POST, GET, PUT, DELETE, OPTIONS",
-          cookieHeader,
-        },
-        statusCode: response.status,
-      }
-    } else {
-      const body = await response.text()
-
-      return {
-        body,
+        cookieHeader,
         statusCode: response.status,
       }
     }
 
+    return {
+      statusCode: 500,
+    }
+  }
+
+  const getCart = async (event) => {
+    console.info("getCart")
+
+    const cartId = event.queryStringParameters?.cartId
+
+    const path = `carts/${cartId}`
+
+    const url = new URL(path, `${apiURL}`)
+
+    url.searchParams.append("include", "redirect_urls")
+
+    const response = await fetch(url, options)
+
+    if (response.ok) {
+      const body = await response.text()
+
+      const { id } = await response.json()
+
+      const cookieHeader = cookie.serialize("cartId", id, {
+        maxAge: 60 * 60 * 24 * 28, // 4 weeks
+      })
+
+      return {
+        body,
+        cookieHeader,
+        statusCode: response.status,
+      }
+    }
+
+    return {
+      statusCode: 500,
+    }
+  }
+
+  const updateCart = async (event) => {
+    console.info("updateCart")
+
+    const cartId = event.queryStringParameters?.cartId
+
+    const path = `carts/${cartId}`
+
+    const url = new URL(path, `${apiURL}`)
+
+    url.searchParams.append("include", "redirect_urls")
+
+    const data = {
+      line_items: [
+        {
+          quantity: 1,
+          product_id: 104,
+        },
+      ],
+    }
+
+    const response = await fetch(url, {...options, body: JSON.stringify(data), method: "POST"})
+
+    if (response.ok) {
+      const body = await response.text()
+
+      const { id } = await response.json()
+
+      const cookieHeader = cookie.serialize("cartId", id, {
+        maxAge: 60 * 60 * 24 * 28, // 4 weeks
+      })
+
+      return {
+        body,
+        cookieHeader,
+        statusCode: response.status,
+      }
+    }
+
+    return {
+      statusCode: 500,
+    }
+  }
+
+  try {
+    // console.log(event)
+
+    if (event.httpMethod === "GET" && event.queryStringParameters?.cartId) {
+      return await getCart(event)
+    }
+
+    if (event.httpMethod === "POST") {
+      if (event.queryStringParameters?.cartId) {
+        return await updateCart(event)
+      } else {
+        return await createCart(event)
+      }
+    }
   } catch (error) {
     console.error(error)
 
