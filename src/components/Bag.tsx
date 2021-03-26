7e7759dac9362cc29b8da8a6a3a8d22dea83a7ba
Karl Podger <karl.podger@primeordinal.com>
2021-03-26 15:10:23 +0000

    Remove commented code from Bag

--- src/components/Bag.tsx
+++ src/components/Bag.tsx
@@ -1,12 +1,7 @@
-import type { Context } from "react"
-
 import type { Bag } from "../../types/BigCommerce"
 
 import React from "react"
 
-// export const BagContext: Context<Bag> = React.createContext({});
-// export const BagContext: Context<Bag> = React.createContext({}) as Context<Bag>
-// export const BagContext: Context<[Bag, () => {}]> = React.createContext([{}, () => {}])
 export const BagContext = React.createContext({
     bag: {},
     setBag: (bag) => { console.log(bag) },
