3ad551a3d8a153884ba684bc513b14e4ccae2f5d
Karl Podger <karl.podger@primeordinal.com>
2021-03-30 23:51:44 +0100

    Add classes to set semantic heading values

--- src/styles/headings.tsx
+++ src/styles/headings.tsx
@@ -9,4 +9,28 @@ export const headings = `
         font-weight: lighter;
         margin-block-start: 0.25em;
     }
+
+    .heading-1 {
+        font-size: var(--font-size-heading-1, 32px);
+    }
+
+    .heading-2 {
+        font-size: var(--font-size-heading-2, 24px);
+    }
+
+    .heading-3 {
+        font-size: var(--font-size-heading-3, 18px);
+    }
+
+    .heading-4 {
+        font-size: var(--font-size-heading-4, 16px);
+    }
+
+    .heading-5 {
+        font-size: var(--font-size-heading-5, 14px);
+    }
+
+    .heading-6 {
+        font-size: var(--font-size-heading-6, 12px);
+    }
 `
